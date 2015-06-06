45c4978761e78cbf586b8b4d18c40d306d688355
meh <meh@schizofreni.co>
2015-06-06 13:17:30 +0200

    lib: add various basic library getters

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#![feature(core)]
+
+extern crate libc;
+extern crate openal_sys as ffi;
+
+use std::ffi::CStr;
+use std::str::from_utf8_unchecked;
+
+use ffi::*;
+
+pub fn vendor() -> Option<&'static str> {
+	unsafe {
+		let ptr = alGetString(AL_VENDOR);
+
+		if ptr.is_null() {
+			None
+		}
+		else {
+			Some(from_utf8_unchecked(CStr::from_ptr(ptr).to_bytes()))
+		}
+	}
+}
+
+pub fn version() -> Option<&'static str> {
+	unsafe {
+		let ptr = alGetString(AL_VERSION);
+
+		if ptr.is_null() {
+			None
+		}
+		else {
+			Some(from_utf8_unchecked(CStr::from_ptr(ptr).to_bytes()))
+		}
+	}
+}
+
+pub fn renderer() -> Option<&'static str> {
+	unsafe {
+		let ptr = alGetString(AL_RENDERER);
+
+		if ptr.is_null() {
+			None
+		}
+		else {
+			Some(from_utf8_unchecked(CStr::from_ptr(ptr).to_bytes()))
+		}
+	}
+}
+
+pub fn extensions() -> Option<&'static str> {
+	unsafe {
+		let ptr = alGetString(AL_EXTENSIONS);
+
+		if ptr.is_null() {
+			None
+		}
+		else {
+			Some(from_utf8_unchecked(CStr::from_ptr(ptr).to_bytes()))
+		}
+	}
+}
