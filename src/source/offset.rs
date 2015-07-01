69495ccc6e9ff5eb0be4f870bdf37baba679b6e8
meh <meh@schizofreni.co>
2015-07-01 22:00:46 +0200

    source/offset: small API improvements

--- src/source/offset.rs
+++ src/source/offset.rs
@@ -6,15 +6,27 @@ pub enum Offset {
 }
 
 impl Offset {
-	pub fn seconds() -> Self {
+	pub fn seconds(value: f32) -> Self {
+		Offset::Seconds(value)
+	}
+
+	pub fn samples(value: f32) -> Self {
+		Offset::Samples(value)
+	}
+
+	pub fn bytes(value: f32) -> Self {
+		Offset::Bytes(value)
+	}
+
+	pub fn as_seconds() -> Self {
 		Offset::Seconds(0.0)
 	}
 
-	pub fn samples() -> Self {
+	pub fn as_samples() -> Self {
 		Offset::Samples(0.0)
 	}
 
-	pub fn bytes() -> Self {
+	pub fn as_bytes() -> Self {
 		Offset::Bytes(0.0)
 	}
 }
